826115b4db2dcc09f3c6e01bd53a4d514f39f8bd
aaklabs <connectakonline@gmail.com>
2025-03-01 18:32:40 -0500

    Setup Postgres using docker

---
+++ db_devops/setup_postgres_using_docker/init/setup-database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (username, email) VALUES ('testuser', 'testuser@example.com');
